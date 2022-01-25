f4db18a267dc9d97a8fce05fcdd62245e6535bf8
Jdavid1002 <rjuandavid1002@gmail.com>
2022-01-25 15:52:16 -0500

    [UPDATE]  added hovers and organization files

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,23 +1,23 @@
 import React from 'react'
-  
+
 const Home = () => {
-    return (
-        <div>
-            <div className="dad-grid" >
-                <div className="grid" >
-                    <div className="grid-child pointer" onClick={()=> window.location.replace("/Fibonacci") }  >
-                        <h4 className="text-center " > Fibonacci </h4>
-                    </div>
-                    <div className="grid-child pointer" onClick={()=> window.location.replace("/AKELAB") }  >
-                        <h4 className="text-center " > Secuencia AKELAB </h4>
-                    </div>
-                    <div className="grid-child pointer" onClick={()=> window.location.replace("/Movies") } >
-                        <h4 className="text-center " > App de Peliculas </h4>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="dad-grid" >
+        <div className="grid" >
+          <div className="grid-child pointer" onClick={()=> window.location.replace("/Fibonacci") }  >
+            <h4 className="text-center " > Fibonacci </h4>
+          </div>
+          <div className="grid-child pointer" onClick={()=> window.location.replace("/AKELAB") }  >
+            <h4 className="text-center " > Secuencia AKELAB </h4>
+          </div>
+          <div className="grid-child pointer" onClick={()=> window.location.replace("/Movies") } >
+            <h4 className="text-center " > App de Peliculas </h4>
+          </div>
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Home;
--- src/views/Movies/Movies.js
+++ src/views/Movies/Movies.js
@@ -1,47 +1,49 @@
-import React,{useEffect} from 'react'
-import axios from 'axios'
-import Nav from './Nav'
-import { useDispatch } from 'react-redux'
-import RenderMovies from './RenderMovies'
-import '../../css/Movies.css'
+import React,{useEffect} from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+import Nav from './Nav';
+import RenderMovies from './RenderMovies';
+
+import '../../css/Movies.css';
 
 
 const Movies = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const getMovies = async () => {
-      const moviesQuerie = await axios({
-        method: 'post',
-        url: 'http://localhost:3001/api/Movies',
-        data: {
-          akelab : "123456789"
-        }
-      });
-      const res = await moviesQuerie.data;
-      const initialState = {
-        genres : res.genres,
-        images_url : res.images_url,
-        n : res.n,
-        results : res.results
-      };
+  const getMovies = async () => {
+    const moviesQuerie = await axios({
+      method: 'post',
+      url: 'http://localhost:3001/api/Movies',
+      data: {
+        akelab : "123456789"
+      }
+    });
+    const res = await moviesQuerie.data;
+    const initialState = {
+      genres : res.genres,
+      images_url : res.images_url,
+      n : res.n,
+      results : res.results
+    };
 
-      dispatch({
-        type : "GET__MOVIES",
-        payload : initialState
-      });
-    }
+    dispatch({
+      type : "GET__MOVIES",
+      payload : initialState
+    });
+  }
 
-    useEffect(() => {
-      getMovies();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  useEffect(() => {
+    getMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-    return (
-      <div className="containerMovies" >
-        <Nav />
-        <RenderMovies />
-      </div>
-    );
+  return (
+    <div className="containerMovies" >
+      <Nav />
+      <RenderMovies />
+    </div>
+  );
 }
 
 export default Movies;
